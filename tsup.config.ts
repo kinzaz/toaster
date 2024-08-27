ad240c7d19b11533898437337f30e8d67beb931d
Владислав <kinzaz@Noutbuk-Vladislav.local>
2024-08-27 13:58:38 +0300

    Исправил конфигурацию сборщика проекта

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.tsx"],
+  format: ["esm", "cjs"],
+  outDir: "dist",
+  dts: true,
   splitting: false,
   sourcemap: true,
   clean: true,
-  dts: true,
 });
